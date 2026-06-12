e5ef0a83aa5ca9e0affdf87eb9c4c06e7d53b3da
Ardiendo <ari@th33g00sth.win>
2026-06-12 05:43:57 +0200

    CyberDeck APP

--- CyberDeck/obj/Release/net10.0-windows/CyberDeck.AssemblyInfo.cs
+++ CyberDeck/obj/Release/net10.0-windows/CyberDeck.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberDeck")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3929bbbc74e3151fd1b018bd8cc18ac813e46e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberDeck")]
--- CyberDeck/obj/Release/net10.0-windows/MainWindow.g.cs
+++ CyberDeck/obj/Release/net10.0-windows/MainWindow.g.cs
@@ -115,7 +115,7 @@ namespace CyberDeck {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CyberDeck;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CyberDeck;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
